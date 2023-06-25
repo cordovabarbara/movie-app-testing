a670c96a83f32cea70d3f75c92d68c94a28ef63a
cordovabarbara <aliendocordova@gmail.com>
2023-06-24 19:01:48 -0500

    create and fix test to actor

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const app = require('./app');
 const sequelize = require('./utils/connection');
 require ("./models")
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 const main = async () => {
     try {
--- src/tests/actor.test.js
+++ src/tests/actor.test.js
@@ -1,24 +1,56 @@
 const request = require ("supertest")
 const app = require("../app")
 
-const URL_BASE = '/api/v1/actors'
+
+const BASE_URL = '/api/v1/actors'
 let actorsId
 
-test ("POST -> 'URL_BASE', should return status code 201 and with correct properties", async() =>{
+test ("POST -> 'BASE_URL', should return status code 201", async() =>{
     const actor = {
-        firstName: "keanu",
-        lastName:"reeve",
+        firstName: "Keanu",
+        lastName:"Reeve",
         nationality:"USA",
         image:"img.pgn",
         birthday:"1964-09-02",
     }
     const res = await request(app)
-        .post(URL_BASE)
+        .post(BASE_URL)
         .send(actor)
 
         actorsId = res.body.id 
-console.log(res)
 
     expect(res.status).toBe(201)
     expect(res.body.firstName).toBe(actor.firstName)
+})
+
+test("GET -> 'BASE_URL', should return status code 200 and res.body.length ==== 1", async()=>{
+    const res = await request(app)
+    .get(BASE_URL)
+
+    expect(res.status).toBe(200)
+    expect(res.body).toHaveLength(1)
+    expect(res.body).toBeDefined()
+
+})
+
+
+test("PUT 'BASE_URL', should return status code 200 and res.body.firstName ==== body.firstName", async()=>{ 
+    const actor = {
+        firstName: "Keanu"
+    }
+
+    const res = await request(app)
+        .put(`${BASE_URL}/${actorsId}`)
+        .send(actor)
+
+    expect(res.status).toBe(200)
+    expect(res.body.firstName).toBe(actor.firstName)
+
+})
+
+test("DELETE 'BASE_URL', should return status code 204", async()=>{ 
+    const res = await request(app)
+        .delete(`${BASE_URL}/${actorsId}`)
+
+    expect(res.status).toBe(204)
 })
